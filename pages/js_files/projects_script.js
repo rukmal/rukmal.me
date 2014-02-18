0bd3b72db29402b208e53a3de54e36841bf6a49c
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-02-18 05:19:23 -0800

    minor changes

--- pages/js_files/projects_script.js
+++ pages/js_files/projects_script.js
@@ -1,25 +1,10 @@
-// /*
-//  * @author Rukmal Weerawarana
-//  *
-//  * Script that contains all javascript functions including jQuery for the
-//  * projects.html page
-//  */
+/*
+ * @author Rukmal Weerawarana
+ *
+ * Script that contains all javascript functions including jQuery for the
+ * projects.html page
+ */
 
-//  $(document).ready(function() {
-//  	$('#listv').click(function() {
-//  		$(this).animate({'background-color':'white'}, 0);
-//  		$(this).data('clicked', true);
-//  		console.log($(this).data('clicked'));
-//  	})
-
-//  	$('#blockv').click(function() {
-//  		$(this).animate({'background-color':'white'});
-//  		$(this).data('clicked', true);
-//  	})
-
-//  	if ($('#listv').data('clicked')) {
-//  		$('#blockv').animate({'backgroud-color':'rgb(104,104,104)'}, 0);
-//  	} else {
-//  		$('#listv').animate({'background-color':'rgb(104,104,104'}, 0);
-//  	}
-//  })
+ $(document).ready(function() {
+ 	
+ })
