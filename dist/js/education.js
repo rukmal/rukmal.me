28d3a6b89f92401aeeee66d70fc2c9926da0bdd4
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-05-04 18:36:10 -0400

    Minor CSS and javascript updates; nothing to see here, incremental updates (border size, etc.)

--- dist/js/education.js
+++ dist/js/education.js
@@ -1,2 +1,13 @@
 current_tab_id = '#education_nav_item';
 updateActiveTab(current_tab_id);
+
+
+function educationDataHandler(store) {
+    // This is working now! Just need to start doing stuff
+    console.log(store);
+    console.log('ehre');
+    console.log(new Date().getTime());
+}
+
+console.log(new Date().getTime());
+loadGraphData(educationDataHandler);
--- dist/js/index.js
+++ dist/js/index.js
@@ -1,9 +1,2 @@
 current_tab_id = '#home_nav_item';
 updateActiveTab(current_tab_id);
-
-// ParticlesJS
-particlesJS.load(
-    'particles-js',
-    'assets/particles.json',
-    function () {}
-);
