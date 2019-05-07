c86a207155a68cef49eee12ef16fd1776f8e2bf2
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-05-07 03:17:19 -0400

    Added scaffolding code to start extracting objects from the graph

--- dist/js/education.js
+++ dist/js/education.js
@@ -2,12 +2,25 @@ current_tab_id = '#education_nav_item';
 updateActiveTab(current_tab_id);
 
 
-function educationDataHandler(store) {
-    // This is working now! Just need to start doing stuff
-    console.log(store);
-    console.log('ehre');
-    console.log(new Date().getTime());
+var store_test;
+
+var output = [];
+
+function storeResults(res) {
+    output.push(res);
 }
 
-console.log(new Date().getTime());
+function onDone() {
+    // Deal with 'output' object here
+    // Need to recursively get all the shit we need
+    // Sorting output 
+    output.sort(compareDataDesc);
+}
+
+function educationDataHandler(store) {
+    // This is working now! Just need to start doing stuff
+    
+    getOfType(store, 'Degree', storeResults, onDone)
+}
+
 loadGraphData(educationDataHandler);
