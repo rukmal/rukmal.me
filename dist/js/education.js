d8ce040ae57d879a3ed86279b1ef916bfd9d92af
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-06-01 01:16:15 -0400

    Notes on the education page; getting back to working on this now

--- dist/js/education.js
+++ dist/js/education.js
@@ -15,12 +15,22 @@ function onDone() {
     // Need to recursively get all the shit we need
     // Sorting output 
     output.sort(compareDataDesc);
+
+    // TODO Now:
+    // Write another sparql query to get all this shit out of the graph, one by one
+    // (i.e. get it for each of the shits in output, in order)
+
+    // Explore adding shit shite directly to the original query; will improve speed
+
+    // An alternative to dealing with Javascript's asnyc clusterfuck would be to put
+    // both of these functions inside the education data handler; then you will have
+    // access to `store`, and can just write your queries directly in there
 }
 
 function educationDataHandler(store) {
     // This is working now! Just need to start doing stuff
     
-    getOfType(store, 'Degree', storeResults, onDone)
+    getOfType(store, 'Degree', storeResults, onDone);
 }
 
 loadGraphData(educationDataHandler);
