0fe66d7476f01a1758350a9d86e4d5263a9210eb
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-06-16 19:12:09 -0400

    Fixed image stuff for layouts

--- dist/js/layouts.js
+++ dist/js/layouts.js
@@ -1,12 +1,30 @@
+function buildOptionalExtResElem(elem, iri) {
+    if (elem[relIRI(iri)]['?obj_ext_res']) {
+        return `<span class="descr_emph" rel="popover" data-dbpedia="${elem[relIRI(iri)]['?obj_ext_res'].value}">${elem[relIRI(iri)]['?obj_name'].value}</span>`
+    } else {
+        return elem[relIRI(iri)]['?obj_name'].value
+    }
+}
+
 function layoutEducation(elem, elem_id) {
     var item = `
-    <div id=${elem_id}>
-        <div class="elem_name">${elem[relIRI('hasName')]['?obj'].value}</div>
-        <div class="deg_department">
-            <span class="descr_emph" rel="popover" data-dbpedia="${(elem[relIRI('degreeDepartment')]['?obj_ext_res']) ? elem[relIRI('degreeDepartment')]['?obj_ext_res'].value : ''}">${elem[relIRI('degreeDepartment')]['?obj_name'].value}</span>
+    <div class="precis_element container">
+    <div id=${elem_id} class="row">
+        <div class="col-sm-8 element_content_container">
+            <div class="elem_secondary">
+                ${buildOptionalExtResElem(elem, 'degreeUniversity')}
+            </div>
+            <div class="elem_third">
+            ${buildOptionalExtResElem(elem, 'degreeSchool')}
+            </div>
+            <div class="elem_name">${elem[relIRI('hasName')]['?obj'].value}</div>
+            <div class="elem_fourth">
+                ${buildOptionalExtResElem(elem, 'degreeDepartment')}
+            </div>
         </div>
-        <div class="deg_school">
-        <span class="descr_emph" rel="popover" data-dbpedia="${(elem[relIRI('degreeSchool')]['?obj_ext_res']) ? elem[relIRI('degreeSchool')]['?obj_ext_res'].value : ''}">${elem[relIRI('degreeSchool')]['?obj_name'].value}</span>
+        <div class="col-sm-4 element_picture_container">
+            <img class="element_picture" src="${elem[relIRI('hasImage')]['?obj'].value}">
+        </div>
     </div>
     </div>
     `
